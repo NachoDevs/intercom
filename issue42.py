b11f0513a1ed617595d6a9b7ed320a0c100cd1c0
NachoDevs <nachodevelopments@gmail.com>
2019-10-31 08:16:11 +0100

    Issue42 iteracion 3, se escucha pero con mucho ruido

--- issue42.py
+++ issue42.py
@@ -13,7 +13,7 @@ class issue42(Intercom_buffer):
 
         Intercom_buffer.init(self, args)
 
-        self.packet_format = f"!HB{self.frames_per_chunk}h" # Indice del chunk; indice de la columna;info
+        self.packet_format = f"!HB{(self.samples_per_chunk//8)//self.number_of_channels}h" # Indice del chunk; indice de la columna;info
         
     # Overriding receive_and_buffer method from parent
     def run(self):
@@ -24,23 +24,15 @@ class issue42(Intercom_buffer):
         # Overriding receive_and_buffer method from parent
         def receive_and_buffer():
             message, source_address = self.receiving_sock.recvfrom(Intercom_buffer.MAX_MESSAGE_SIZE)
-            chunk_number, column_index, *bp = struct.unpack(self.packet_format, message)
+            chunk_number, bitplane_index, *bp = struct.unpack(self.packet_format, message)
 
-            # to_reproduce = np.asarray(bp, np.int16)
-
-
-            bp_array = np.asarray(bp, dtype=np.uint8)
+            bp_array = np.asarray(bp, np.uint8)
 
             unpacked_bp = np.unpackbits(bp_array)
             
             to_reproduce = unpacked_bp.astype(np.int16)
 
-
-            self._buffer[chunk_number % self.cells_in_buffer][:, column_index % 2] |= (to_reproduce << column_index//2)
-
-            # print(column_index)
-
-            # self.recieved_column_number %= (self.bits_per_number * self.number_of_channels)
+            self._buffer[chunk_number % self.cells_in_buffer][:, bitplane_index % 2] |= (to_reproduce << bitplane_index)
 
             return chunk_number
 
@@ -52,19 +44,19 @@ class issue42(Intercom_buffer):
             print("Antes de enviar:")
             print(data)
 
-            column_index = self.bits_per_number - 1
-            while column_index >= 0:
-                channel_index = 0
+            bitplane_index = self.bits_per_number - 1
+            while bitplane_index >= 0:
                 for channel_index in range(self.number_of_channels):
-                    bp = data[:, channel_index] >> column_index & 1
+                    bp = data[:, channel_index] >> bitplane_index & 1
 
+                    # Parsing our int16 into uint8 for the packbits input
                     packed_bp = np.packbits(bp.astype(np.uint8))
 
-                    to_send = struct.pack(self.packet_format, self.recorded_chunk_number, column_index, *(packed_bp))
+                    to_send = struct.pack(self.packet_format, self.recorded_chunk_number, bitplane_index, *(packed_bp))
 
                     self.sending_sock.sendto(to_send, (self.destination_IP_addr, self.destination_port))
 
-                    column_index -= 1
+                bitplane_index -= 1
 
             self.recorded_chunk_number = (self.recorded_chunk_number + 1) % self.MAX_CHUNK_NUMBER
 

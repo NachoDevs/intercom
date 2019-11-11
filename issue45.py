917b9fd69214ac67a75711794f3f2c96366b681a
Ignacio Aguilera Gómez <aguileragomezignacio@gmail.com>
2019-11-11 10:58:27 +0100

    Subiendo solucion adaptada a intercom_bitplanes.

--- issue45.py
+++ issue45.py
@@ -1,73 +1,49 @@
 import sounddevice as sd
 import numpy as np
 import struct
-from issue42 import issue42
+from intercom_bitplanes import Intercom_bitplanes
 
 if __debug__:
     import sys
 
-class issue45(issue42):
+class issue45(Intercom_bitplanes):
 
     def init(self, args):
-        issue42.init(self, args)
+        Intercom_bitplanes.init(self, args)
+        if self.number_of_channels == 2:
+            self.record_send_and_play = self.record_send_and_play_stereo
+        # elif self.number_of_channels > 2:
+        #     self.record_send_and_play = self.record_send_and_play_extra
 
-    # Overriding receive_and_buffer method from parent
-    def run(self):
+    # Used when we have 2 channels
+    def record_send_and_play_stereo(self, indata, outdata, frames, time, status):        
+        indata[:, 0] -= indata[:, 1] 
+        self.send(indata)
+        
+        self.recorded_chunk_number = (self.recorded_chunk_number + 1) % self.MAX_CHUNK_NUMBER
+        chunk = self._buffer[self.played_chunk_number % self.cells_in_buffer]
+        chunk[:, 0] += chunk[:, 1]
+        
+        self._buffer[self.played_chunk_number % self.cells_in_buffer] = self.generate_zero_chunk()
+        self.played_chunk_number = (self.played_chunk_number + 1) % self.cells_in_buffer
+        outdata[:] = chunk
+        if __debug__:
+            sys.stderr.write("."); sys.stderr.flush()
 
-        self.recorded_chunk_number = 0
-        self.played_chunk_number = 0
-
-        # Overriding receive_and_buffer method from parent
-        def receive_and_buffer():
-            message, source_address = self.receiving_sock.recvfrom(issue42.MAX_MESSAGE_SIZE)
-            chunk_number, bitplane_index, *bp = struct.unpack(self.packet_format, message)
-
-            unpacked_bp = np.unpackbits(np.asarray(bp, np.uint8))
-
-            to_reproduce = unpacked_bp.astype(np.int16)
-
-            self._buffer[chunk_number % self.cells_in_buffer][:, bitplane_index % self.number_of_channels] |= (to_reproduce << (bitplane_index // 2))
-
-            return chunk_number
-
-        # Overriding recond_send_and_play method from parent
-        def record_send_and_play(indata, outdata, frames, time, status):
-
-            data = np.frombuffer(indata, np.int16).reshape(self.frames_per_chunk, self.number_of_channels)
-
-            data[:, 0] -= data[:, 1] 
-
-            bitplane_index = (self.bits_per_number * self.number_of_channels) - 1
-            while bitplane_index >= 0:
-                bp = data[:, bitplane_index % self.number_of_channels] >> (bitplane_index // 2) & 1
-
-                packed_bp = np.packbits(bp.astype(np.uint8))
-
-                to_send = struct.pack(self.packet_format, self.recorded_chunk_number, bitplane_index, *(packed_bp))
-
-                self.sending_sock.sendto(to_send, (self.destination_IP_addr, self.destination_port))
-
-                bitplane_index -= 1
-
-            self.recorded_chunk_number = (self.recorded_chunk_number + 1) % self.MAX_CHUNK_NUMBER
-
-            chunk = self._buffer[self.played_chunk_number % self.cells_in_buffer]
-            
-            chunk[:, 0] += chunk[:, 1]
-
-            self._buffer[self.played_chunk_number % self.cells_in_buffer] = self.generate_zero_chunk()
-            self.played_chunk_number = (self.played_chunk_number + 1) % self.cells_in_buffer
-            outdata[:] = chunk
-            if __debug__:
-                sys.stderr.write("."); sys.stderr.flush()
-
-
-        with sd.Stream(samplerate=self.frames_per_second, blocksize=self.frames_per_chunk, dtype=np.int16, channels=self.number_of_channels, callback=record_send_and_play):
-            print("-=- Press CTRL + c to quit -=-")
-            first_received_chunk_number = receive_and_buffer()
-            self.played_chunk_number = (first_received_chunk_number - self.chunks_to_buffer) % self.cells_in_buffer
-            while True:
-                receive_and_buffer()
+    # Used when we have more than 3 channels
+    # def record_send_and_play_extra(self, indata, outdata, frames, time, status):
+    #     indata[:, 0] -= indata[:, 1] 
+    #     self.send(indata)
+        
+    #     self.recorded_chunk_number = (self.recorded_chunk_number + 1) % self.MAX_CHUNK_NUMBER
+    #     chunk = self._buffer[self.played_chunk_number % self.cells_in_buffer]
+    #     chunk[:, 0] += chunk[:, 1]
+        
+    #     self._buffer[self.played_chunk_number % self.cells_in_buffer] = self.generate_zero_chunk()
+    #     self.played_chunk_number = (self.played_chunk_number + 1) % self.cells_in_buffer
+    #     outdata[:] = chunk
+    #     if __debug__:
+    #         sys.stderr.write("."); sys.stderr.flush()
 
 if __name__ == "__main__":
     issue45 = issue45()

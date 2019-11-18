13845f1eda22cad03b9198d1b90f2124b58a6078
NachoDevs <nachodevelopments@gmail.com>
2019-11-18 19:09:59 +0100

    Iteracion 2 issue44, media aritmetica para regular los picos.

--- issue44.py
+++ issue44.py
@@ -12,7 +12,12 @@ class issue44(Intercom_binaural):
         Intercom_binaural.init(self, args)
         self.packet_format = f"!HBB{self.frames_per_chunk//8}B" # Chunk index; Column index; Bitplanes to send; Data
         self.bitplanes_to_send = self.number_of_channels * 16
-        self.received_bitplanes = 0
+
+        self.to_send_mean = self.bitplanes_to_send
+
+        self.received_bitplanes = dict([])
+        for i in range(self.cells_in_buffer):
+            self.received_bitplanes[i] = self.bitplanes_to_send
 
     def receive_and_buffer(self):
         message, source_address = self.receiving_sock.recvfrom(Intercom_binaural.MAX_MESSAGE_SIZE)
@@ -20,11 +25,13 @@ class issue44(Intercom_binaural):
         bitplane = np.asarray(bitplane, dtype=np.uint8)
         bitplane = np.unpackbits(bitplane)
         bitplane = bitplane.astype(np.int16)
-        self.received_bitplanes += 1
+
+        self.received_bitplanes[chunk_number % self.cells_in_buffer] += 1
+
         self._buffer[chunk_number % self.cells_in_buffer][:, bitplane_number%self.number_of_channels] |= (bitplane << bitplane_number//self.number_of_channels)
         return chunk_number
 
-    def record_send_and_play_stereo(self, indata, outdata, frames, time, status):        
+    def record_send_and_play_stereo(self, indata, outdata, frames, time, status):
         indata[:, 0] -= indata[:, 1] 
         self.send(indata)
         
@@ -32,9 +39,15 @@ class issue44(Intercom_binaural):
         chunk = self._buffer[self.played_chunk_number % self.cells_in_buffer]
         chunk[:, 0] += chunk[:, 1]
 
-        self.bitplanes_to_send = self.received_bitplanes
-        self.received_bitplanes = 0  
-        print(self.bitplanes_to_send)
+        self.bitplanes_to_send = self.received_bitplanes[self.played_chunk_number % self.cells_in_buffer]
+        
+        # Convex combination
+        self.to_send_mean = int((self.received_bitplanes[(self.played_chunk_number - 1) % self.cells_in_buffer] * 0.4) \
+                            + (self.bitplanes_to_send * 0.6))
+
+        self.received_bitplanes[(self.played_chunk_number - 1) % self.cells_in_buffer] = 0
+
+        print(self.to_send_mean)
         self._buffer[self.played_chunk_number % self.cells_in_buffer] = self.generate_zero_chunk()
         self.played_chunk_number = (self.played_chunk_number + 1) % self.cells_in_buffer
         outdata[:] = chunk
@@ -42,7 +55,7 @@ class issue44(Intercom_binaural):
             sys.stderr.write("."); sys.stderr.flush()
 
     def send(self, indata):
-        for bitplane_number in range(self.bitplanes_to_send - 1, -1, -1):
+        for bitplane_number in range(self.to_send_mean - 1, -1, -1):
             bitplane = (indata[:, bitplane_number%self.number_of_channels] >> bitplane_number//self.number_of_channels) & 1
             bitplane = bitplane.astype(np.uint8)
             bitplane = np.packbits(bitplane)
